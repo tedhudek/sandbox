6010f71872efb96353dc4eec63431a0fb1f71574
Ted Hudek <tedhudek@gmail.com>
2018-01-01 12:28:48 -0800

    store power factors in list

--- test-binary.py
+++ test-binary.py
@@ -1,5 +1,7 @@
-# 12/31/17
+# 1/1/18
 # Base Conversion Program
+
+power_factors=[]
 
 def test_pow(testnum) : # return true if yes, false if no
     while testnum==int(testnum) :
@@ -17,17 +19,22 @@ def test_pow(testnum) : # return true if yes, false if no
 
 # input a number
 
-starting_num=1000000
+starting_num=150
 
 while starting_num>0 :
     num=starting_num
     while test_pow(float(num))==False :
         #print('We tested '),num,(' and it failed.')
         num=num-1
-    print('Found a power! It is '),num
+    #print('Found a power! It is '),num
+    power_factors.append(num)
     starting_num=starting_num-num
 
+print(power_factors)
 
 # next steps: output binary values instead of power factors
 # take keyboard input
 # change base
+
+# current output: 64,4,2. set pointer to right-most value.
+# test 1,2,4,8,16,32,64,128...
