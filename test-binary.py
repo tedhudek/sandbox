73067048f7a0e37d6c814480ef57a430317ce04d
Ted Hudek <tedhudek@gmail.com>
2018-01-20 12:48:43 -0800

    added string formatting and loaded power factors list

--- test-binary.py
+++ test-binary.py
@@ -1,29 +1,22 @@
 # 1/16/18
 # Base Conversion Program
 
-#power_factors=[]
-BASE=16
+BASE=2
 factor_counts=[]
 
 def test_pow(testnum) : # return true if yes, false if no
     num_total_divides=0
     while testnum==int(testnum) :
-        #print testnum,('is an integer value so we are starting the while loop.')
-        #print('Type of testnum is'),type(testnum)
         if testnum==1 :
-            #print testnum,('is one, so returning true from function.')
             return num_total_divides
         else :
-            #print testnum,('is not one, so dividing it by 2.')
             testnum=testnum/BASE
             num_total_divides=num_total_divides+1
-            #print ('Value is now'),testnum           
-    #print testnum,('is not an integer value so returning failure.')
     return -1
 
 # input a number
 
-starting_num=34
+starting_num=20
 
 while starting_num>0 :
     num=starting_num
@@ -31,29 +24,24 @@ while starting_num>0 :
         x=test_pow(num)
         if x != -1 : # break out because num is a power
             break
-        # otherwise say we failed and test the next number
-        # print('We tested '),
-        # print(num)
-        # print(' and it failed.')
         num=num-1
-    print('Found a power! It is '),
-    print(num)
-    print('And the exponent is '),
-    print(x)
-    #if len(factor_counts)==0 :
-    #power_factors.append(num)
+    print('Found a power! It is %i and the exponent is %i'%(num,x))
+    if not factor_counts:
+        # list is empty so let's initialize it
+        factor_counts=[0] * x
+        factor_counts.append(1)
+    else:
+        # list is not empty, so add one to the proper column
+        factor_counts[x]=factor_counts[x]+1
     starting_num=starting_num-num
 
-#print(power_factors)
-#number_of_factors=len(power_factors)
 
+print(factor_counts)
 
 # next steps: output binary values instead of power factors
 # take keyboard input
 # change base
 
-# power factors: 128, 16, 4, 2. set pointer to right-most value.
-# test 1, 2, 4, 8, 16...
 # 530: 256, 256, 16, 1, 1
 # 0x212 (2 in 256's column, 1 in 16's column, 2 in 1's column)
 
