4a814b1c94daa392d7c317c4ce1c4eed8314d1d1
Ted Hudek <tedhudek@gmail.com>
2018-02-10 16:04:38 -0800

    add docstrings, restore text-based (non-GUI) output

--- test-binary.py
+++ test-binary.py
@@ -1,5 +1,5 @@
 
-# 2/3/18
+# 2/10/18
 # Base Conversion Program
 
 import time
@@ -11,10 +11,12 @@ starting_num=46
 s=starting_num # make a copy for final display purposes
 factor_counts=[]
 final_result=''
-root=Tk()
+#root=Tk()
 
 def test_pow(testnum) : # return true if yes, false if no
-    # keep dividing by BASE and return true if we get to one
+    """
+    Keep dividing by BASE and return the exponent if we get to one or negative one if not.
+    """
     num_total_divides=0
     while testnum==int(testnum) :
         if testnum==1 :
@@ -25,10 +27,13 @@ def test_pow(testnum) : # return true if yes, false if no
     return -1
 
 def convert_to_value(digit):
-    # digit is greater than 9, so convert it to a character, starting with A for 10
+    """
+    If digit is greater than 9, so convert it to a character, starting with A for 10.
+    """
     global final_result
     digit=digit+55
     final_result=final_result+chr(digit)
+
 
 while starting_num>0 :
     num=starting_num
@@ -56,13 +61,16 @@ for digit in factor_counts:
     else:
         final_result=final_result+str(digit)
 
-Label(root, text='Converted %i to base %i'%(s,BASE)).pack()
-Label(root, text='Result is %s'%final_result).pack()
+print('Converted %i to base %i'%(s,BASE))
+print('Result is %s'%final_result)
+
+#Label(root, text='Converted %i to base %i'%(s,BASE)).pack()
+#Label(root, text='Result is %s'%final_result).pack()
 #print(final_result)
 #time_string="--- %s seconds ---" % (time.time() - start_time)
 #Label(root, text=time_string).pack()
 
-root.mainloop()
+#root.mainloop()
 
 # take keyboard input
 # then display each digit in concatenated form
