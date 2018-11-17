1b752de1ab0d6e4343d0e4ab0016d646cb197a3d
Ted Hudek <tedhudek@gmail.com>
2018-11-17 12:59:18 -0800

    update to one roller logic

--- play-pig.py
+++ play-pig.py
@@ -4,14 +4,18 @@
 from random import randint
 
 total_points=0
+v=0 #number of turns
+z=0 #total score
 
-def roll() :
+def roll(x,y) :
     x=randint(1,6)
     y=randint(1,6)
-    print('You rolled %i and %i'%(x,y))
-    if((x==1) or (y==1)) :
-        print('You lose')
-    else :
-        print('You win')
 
-roll()
+roll(x,y)
+if((x==1) or (y==1)) :
+    #w stays zero, add w to z
+else :
+    #x+y=w, add w to z
+v++
+#z==100?yes: print(z) no: repeat starting with roll
+#print('You rolled %i and %i'%(x,y))
