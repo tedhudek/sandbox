29b57292afce1cd249a044208fdff6713f04350c
Ted Hudek <tedhudek@gmail.com>
2018-11-23 17:45:52 -0800

    working code for single game with one roller strategy

--- play-pig.py
+++ play-pig.py
@@ -3,21 +3,38 @@
 
 from random import randint
 
-total_points=0
-v=0 #number of turns
+w=0 #points on current turn (that would be lost if 1 is rolled)
+v=1 #number of turns
 z=0 #total score
+max_rolls_per_turn=1
 
 def roll() :
     return randint(1,6)
 
-x=roll()
-y=roll()
+while(1) :
+    current_roll_num=1
+    w=0
 
-print(x,y)
+    x=roll()
+    y=roll()
 
-#if((x==1) or (y==1)) :
-    #w stays zero, add w to z
-#else :
-    #x+y=w, add w to z
-#v++
+    print(x,y)
+
+    if((x==1) or (y==1)) :
+        print('End of turn %i due to 1 rolled'%v)
+        print('Total remains %i'%z)
+        v=v+1
+        continue
+
+    else :
+        w=x+y
+    
+    if(current_roll_num==max_rolls_per_turn) :
+        print('Stopping turn voluntarily, adding %i to %i'%(w,z))
+        z=z+w
+        print('Total is now %i after %i turns'%(z,v))
+        if(z>99) :
+            break
+        v=v+1
+
 #z==100?yes: print(z) no: repeat starting with roll
