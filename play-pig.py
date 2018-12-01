d8b862ca2dfb98e3ccd74f6da210121e132d7a3b
Ted Hudek <tedhudek@gmail.com>
2018-12-01 15:54:48 -0800

    introduced a bug

--- play-pig.py
+++ play-pig.py
@@ -3,19 +3,23 @@
 
 from random import randint
 
+g=0 #current game number
 
 def roll() :
     return randint(1,6)
 
-while(1) :
+while(g<100) : # start a new game
+
+    g=g+1
     w=0 #points on current turn (that would be lost if 1 is rolled)
     v=1 #number of turns
-    z=0 #total score
+    z=0 #total score for the current game
     max_rolls_per_turn=1
+    current_roll_num=0
 
-    while(1) :
-        current_roll_num=1
+    while(1) : # roll again (is this a roll or a turn?)
         w=0
+        current_roll_num=current_roll_num+1
 
         x=roll()
         y=roll()
@@ -39,3 +43,4 @@ while(1) :
                 print('Won after %i turns'%v)
                 break
             v=v+1
+
