4e1a888d7fa466230a5dfa1a90d1a4ffdaa68449
Ted Hudek <tedhudek@gmail.com>
2018-12-08 13:24:21 -0800

    working code to caculate woodly numbers i dunno what there actuly called

---
+++ calc-woodly.py
@@ -0,0 +1,8 @@
+# calculate Woodly numbers
+#12/8/2018
+n=int(input("Please enter a number to see the corrisponding woodly number"))
+p=n*2**n
+a=p-1
+print("The woodly number corresponding to",n,"is",a)   
+
+
