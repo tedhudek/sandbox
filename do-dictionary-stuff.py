ad99fe3e012c3b36c4db66ffe20300ef8765beb7
Ted Hudek <tedhudek@gmail.com>
2019-02-16 17:49:01 -0800

    add code

---
+++ do-dictionary-stuff.py
@@ -0,0 +1,7 @@
+print("Hello world!")
+from PyDictionary import PyDictionary
+
+print("We finished the import. Woo!")
+dictionary=PyDictionary()
+#print (dictionary.meaning("indentation"))
+print(noun)
