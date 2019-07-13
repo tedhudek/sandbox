f8f266ad7eef343eeaed0891e3f17c88c89fc97f
Ted Hudek <tedhudek@gmail.com>
2019-07-13 13:19:42 -0700

    working bubble sort code!

---
+++ bubble_sort.py
@@ -0,0 +1,16 @@
+numbers=[69,43,5,21,9]
+
+for i in range(len(numbers)):
+    # find smallest element, starting from element i
+    smallest=1000
+    for j in range(i,len(numbers)):
+        if(numbers[j]<smallest):
+            smallest=numbers[j]
+            index_of_smallest=j
+    # swap smallest with item i
+    tmp=numbers[i]
+    numbers[i]=numbers[index_of_smallest]
+    numbers[index_of_smallest]=tmp
+    
+    print('Finished pass i= ',i,' and list looks like this:')
+    print(numbers)
