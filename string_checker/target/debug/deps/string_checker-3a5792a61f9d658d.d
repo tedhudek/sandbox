a44acaceb58ec955e5e7aa5a44cc5e26fe599248
Ted Hudek <tedhudek@gmail.com>
2025-04-06 17:29:19 -0700

    remove target, update gitignore

--- string_checker/target/debug/deps/string_checker-3a5792a61f9d658d.d
+++
@@ -1,5 +0,0 @@
-C:\repo\sandbox\string_checker\target\debug\deps\libstring_checker-3a5792a61f9d658d.rmeta: src\main.rs
-
-C:\repo\sandbox\string_checker\target\debug\deps\string_checker-3a5792a61f9d658d.d: src\main.rs
-
-src\main.rs:
--- string_checker/target/debug/string_checker.d
+++
@@ -1,1 +0,0 @@
-C:\repo\sandbox\string_checker\target\debug\string_checker.exe: C:\repo\sandbox\string_checker\src\main.rs
